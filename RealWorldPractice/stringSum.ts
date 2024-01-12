88bd010b91007e8d5eb03916942616c24f14335b
Samuel Wemimo <wemsamuel@gmail.com>
2024-01-12 15:35:33 -0700

    new: simple string sum

---
+++ RealWorldPractice/stringSum.ts
@@ -0,0 +1,15 @@
+// Challenge
+// Assign every lowercase letter a value, from 1 for a to 26 for z. Given a string of lowercase letters, find the sum of the values of the letters in the string.
+
+// lettersum("") => 0
+// lettersum("a") => 1
+// lettersum("z") => 26
+// lettersum("cab") => 6
+// lettersum("excellent") => 100
+// lettersum("microspectrophotometries") => 317
+
+let sum = (s: string): number => {
+  return s.split('').reduce((sum, curr) => sum + curr.charCodeAt(0) - 96, 0);
+};
+
+console.log(sum('microspectrophotometries'));
